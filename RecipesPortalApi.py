5a3cbd4b6a51dfe3f48e9144a0ad1e268c68cb07
Michał <michal.rosiol@gmail.com>
2021-05-20 23:59:42 +0200

    read and write json data

--- RecipesPortalApi.py
+++ RecipesPortalApi.py
@@ -20,20 +20,36 @@ def hello_world():
     return 'Hello, World!'
 
 
-numbers = [1, 2, 3, 4]
+with open(os.path.join(sys.path[0], 'recipes.json'), 'r') as f:
+    data = json.load(f)
+    recipeKeys = [int(i) for i in list(data.keys())]
 
 
 @app.route('/recipe/<number>')
 def index(number):
-    if number.isnumeric() and int(number) in numbers:
-        with open(os.path.join(sys.path[0], number+'.json'), 'r') as f:
+    if number.isnumeric() and int(number) in recipeKeys:
+        with open(os.path.join(sys.path[0], 'recipes.json'), 'r') as f:
             data = json.load(f)
         if 'Content-Type' in request.headers and request.headers['Content-Type'] == 'application/json':
-            return data
+            return data[str(number)]
         else:
-            return json2html.convert(json=data)
+            return '<!DOCTYPE html><html><head><title>Page Title</title></head><body>'+json2html.convert(json=data[str(number)])+'</body></html>'
     else:
         abort(400)
+
+
+@app.route('/recipe/api/new', methods=['POST'])
+def new():
+    content = request.json
+    with open(os.path.join(sys.path[0], 'recipes.json'), 'r+') as f:
+        data = json.load(f)
+        position = {str(max(recipeKeys)+1): content}
+        data.update(position)
+        recipeKeys.append(max(recipeKeys)+1)
+        f.truncate(0)
+        f.seek(0)
+        json.dump(data, f)
+    return jsonify({"id": max(recipeKeys)})
 
 
 if __name__ == '__main__':
